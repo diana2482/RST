36ed6fd1f503e6a5154ad69936d7d6c054e83b0d
diana2482 <orsolyovadianka@gmail.com>
2024-02-13 23:40:34 +0100

    add: router, basic page layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* TODO: add background image */
+  /* background-color: #F5F6F7; */
+  font-family: Poppins, sans-serif;
+  font-size: 1rem;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+
+a {
+  text-decoration: none;
+  color: white;
 }
 </style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="main-page-image-background">a
+    <div class="main-logo-text">
+      <div class="main-logo-text-element">rychlejsie</div>
+      <div class="main-logo-text-element">rychlejsie</div>
+      <div class="main-logo-text-element">rychlejsie</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.main-page-image-background{
+  display: flex;
+  width: 1920px;
+  height: 1080px;
+  /* height: 67.5rem; */
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  background-image: url('../assets/Header_computer.jpg');
+  background-size: cover;
+  background-position: center;
+}
+.main-logo-text{
+  display: flex;
+height: 17.3125rem;
+flex-direction: column;
+justify-content: center;
+align-items: flex-start;
+gap: -2.8125rem;
+position: absolute;
+}
+.main-logo-text-element{
+  color: #FFF;
+-webkit-text-stroke-width: 0.30000001192092896;
+-webkit-text-stroke-color: #FFF;
+font-family: Poppins, sans-serif;
+font-size: 4.6875rem;
+font-style: normal;
+font-weight: 500;
+line-height: 150.667%; /* 7.0625rem */
+letter-spacing: -0.32813rem;
+/*here is a line of code that makes all text uppercase*/
+text-transform: uppercase;
+}
+</style>
