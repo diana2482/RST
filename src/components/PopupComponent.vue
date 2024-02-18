3e3dc045b55e1708fbb8cf7fd8b675ec6f3c0a15
diana2482 <orsolyovadianka@gmail.com>
2024-02-18 23:38:57 +0100

    add: spacer, popup - basic structure without interactions

---
+++ src/components/PopupComponent.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="popup-background">
+        <img class="popup-image" src="../assets/game_days.png"/>
+        <div class="popup-text">
+            Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet 
+        </div>
+        <a class="popup-link">gamedays.sk</a>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PopupComponent',
+}
+</script>
+
+<style>
+.popup-background {
+    display: flex;
+    width: 16rem;
+    padding: 1.25rem;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 2.1875rem;
+    border-radius: 1.875rem;
+    border: 1px solid #FFF;
+    background: rgba(235, 0, 255, 0.16);
+
+    z-index: 1000;
+}
+.popup-image {
+    height: 6.6875rem;
+    align-self: stretch;
+    /* background: url("../assets/game_days.png"), lightgray 10% / cover no-repeat; */
+}
+.popup-text {
+    align-self: stretch;
+    text-align: center;
+    font-size: 1.375rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 150.667%; /* 2.07169rem */
+    letter-spacing: -0.06875rem;
+}
+.popup-link {
+    align-self: stretch;
+    text-align: center;
+    font-size: 1.375rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 150.667%; /* 2.07169rem */
+    letter-spacing: -0.06875rem;
+}
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -14,27 +14,41 @@
     </div>
   </div>
 
-  <div id="services" class="main-page-buttons" style="margin-bottom: 11.56rem;">
+  <div id="services" class="main-page-buttons">
     <MainPageButton imageSrc="T_oprava_pocitacov a notebookov.png"
     text="mainPage.opravaPC" :page="'oprava-pc-notebook-kosice'" topMargin="19rem" />
     <MainPageButton imageSrc="T_skladanie_pocitacov.png" text="mainPage.skladaniePC" :page="'novy-pc-kosice'" topMargin="19rem" />
     <MainPageButton imageSrc="T_Diagnostika.png" text="mainPage.diagnostika" :page="'servis-pc-notebook-kosice'" topMargin="19rem" />
   </div>
+  <SpacerComponent :space="'11.56rem'" />
   <div class="main-page-buttons">
     <MainPageButton imageSrc="T_PC_Zostavy.png" text="mainPage.pcZostavy" page="pc-kosice" />
 
     <MainPageButton imageSrc="T_Notebooky.png" text="mainPage.notebooky" :page="'notebook-kosice'" />
     <MainPageButton imageSrc="T_Portfólio.png" text="mainPage.portfolio" :page="'portfolio-pc-servis-kosice'" />
   </div>
+
+  <SpacerComponent :space="'5rem'" />
+
+  <div class="partners">
+    <img src="../assets/game_days.png" class="partner"/>
+    <img src="../assets/games_farm.png" class="partner"/>
+    <img src="../assets/tripple_hill.png" class="partner"/>
+  </div>
+  <PopupComponent />
 </template>
 
 <script>
 import MainPageButton from '../components/MainPageButton.vue';
+import SpacerComponent from '../components/SpacerComponent.vue';
+import PopupComponent from '@/components/PopupComponent.vue';
 
 export default {
   name: 'MainPage',
   components: {
     MainPageButton,
+    SpacerComponent,
+    PopupComponent,
   },
 }
 
@@ -75,7 +89,6 @@ export default {
   align-items: flex-start;
   height: 100%;
 }
-
 .main-logo-text-element {
   text-shadow: 1px 6px 4px rgba(0, 0, 0, 0.50);
   font-style: normal;
@@ -85,7 +98,6 @@ export default {
   /* margin-top: -2.8125rem; */
   margin: 0;
 }
-
 .main-page-buttons {
   display: flex;
   justify-content: center;
@@ -94,5 +106,19 @@ export default {
   gap: 2.5rem;
   width: 45rem;
   margin: 0 auto;
+}
+.partners{
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 1.5rem;
+}
+
+.partner{
+  display: flex;
+  width: 18.75rem;
+  height: 7.75406rem;
+  justify-content: center;
+  align-items: center;
 }
 </style>
