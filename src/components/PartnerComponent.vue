40cebb4268a814b2be565864ef295741da8d7760
Diana <diana.orsolyova@student.tuke.sk>
2024-02-27 15:59:48 +0100

    adjusted partners, need to remove the popup component

--- src/components/PartnerComponent.vue
+++ src/components/PartnerComponent.vue
@@ -1,17 +1,13 @@
 <template>
     <div class="partner-container" @click="togglePopup">
-        <Transition name="fade">
-            <PopupComponent v-if="isPopupVisible" :text="translatedText" :page="page" class="popup" />
-        </Transition>
         <img :src="require('@/assets/' + image)" class="partner" />
+        <div>{{ translatedText }}</div>
+        <a class="partner-link" :href="formattedPage" target="_blank">{{ page }}</a>
     </div>
+    
 </template>
 
-
 <script>
-import PopupComponent from '@/components/PopupComponent.vue';
-import { eventBus } from '@/eventBus.js';
-
 export default {
     props: {
         image: {
@@ -27,50 +23,18 @@ export default {
             required: true,
         },
     },
-    data() {
-        return {
-            isPopupVisible: false,
-        };
-    },
     computed: {
         translatedText() {
             return this.$t(this.text);
         },
-    },
-    methods: {
-        togglePopup(event) {
-            event.stopPropagation(); // Prevent click event from bubbling to document
-            if (!this.isPopupVisible) {
-                eventBus.emit('close-all-popups');
-                this.isPopupVisible = true;
-                // Add global click listener only when popup becomes visible
-                document.addEventListener('click', this.handleDocumentClick);
-            } else {
-                this.isPopupVisible = false;
+        formattedPage() {
+            if (!/^https?:\/\//i.test(this.page)) {
+                return `https://${this.page}`;
             }
+            return this.page;
         },
-        closePopup() {
-            this.isPopupVisible = false;
-            document.removeEventListener('click', this.handleDocumentClick); // Clean up
-        },
-        handleDocumentClick(event) {
-            // Check if the click is outside the popup
-            if (!this.$el.contains(event.target)) {
-                eventBus.emit('close-all-popups');
-            }
-        }
-    },
-    mounted() {
-        eventBus.on('close-all-popups', this.closePopup);
-    },
-    beforeUnmount() {
-        eventBus.off('close-all-popups', this.closePopup);
-        document.removeEventListener('click', this.handleDocumentClick); // Clean up
     },
     name: 'PartnerComponent',
-    components: {
-        PopupComponent,
-    },
 }
 </script>
 
@@ -79,31 +43,14 @@ export default {
     position: relative;
     width: 16.875rem;
     height: auto;
+    text-align: justify;
 }
-
 .partner {
     width: 100%;
     height: 100%;
-    cursor: pointer;
 }
-
-.popup {
-    position: absolute;
-    top: 0;
-    left: 50%;
-    transform: translate(-50%, -1.5rem);
-    transition: opacity 3s ease;
-}
-
-/* Transition styles */
-.fade-leave-active,
-.fade-enter-active {
-    transition: opacity 0.4s ease;
-}
-
-.fade-enter,
-.fade-leave-to {
-    opacity: 0;
+.partner-link {
+    float: center !important;
 }
 @media (max-width: 900px) {
     .partner-container {
