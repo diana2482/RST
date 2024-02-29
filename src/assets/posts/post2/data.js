dfc79650ad921e79955008c513d22e1ea825cccc
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-02-29 12:34:04 +0100

    post2 add

--- src/assets/posts/post2/data.js
+++ src/assets/posts/post2/data.js
@@ -1,14 +1,14 @@
 // src/assets/posts/post2/data.js
 export default {
     sk: {
-      title: "druhy post",
-      date: "2016-01-01",
-      description: "Toto je druhy post",
+      title: "Upgrade RAM a kompletná profylaxia",
+      date: "2023-12-15",
+      description: "Potreboval by som rozšíriť RAM. Myslím že prečistiť ho popri tom by bolo určite fajn, ako je to u vás? :)",
     },
     en: {
-      title: "2nd post",
-      date: "2016-01-01",
-      description: "This is the 2nd post",
+      title: "RAM upgrade with full maintenance.",
+      date: "2023-12-15",
+      description: "I need to expand my RAM. I think it would be great to clean it while we're at it.",
     }
   };
   
